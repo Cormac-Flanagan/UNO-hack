8155e7e6c4362d0819d694c1ad44189c701ae962
cormac <cormacflanagan12@gmail.com>
2024-08-17 16:35:14 +1000

    pulled

---
+++ hello-cargo/target/debug/deps/hello_cargo-2632a4733157fc09.d
@@ -0,0 +1,5 @@
+/home/cormac/Projects/Rust/Hackathon/UNO-GT/hello-cargo/target/debug/deps/libhello_cargo-2632a4733157fc09.rmeta: src/main.rs
+
+/home/cormac/Projects/Rust/Hackathon/UNO-GT/hello-cargo/target/debug/deps/hello_cargo-2632a4733157fc09.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/deps/hello_cargo-7fb17f453b82d30a.d
@@ -0,0 +1,5 @@
+/home/cormac/Projects/Rust/Hackathon/UNO-GT/hello-cargo/target/debug/deps/libhello_cargo-7fb17f453b82d30a.rmeta: src/main.rs
+
+/home/cormac/Projects/Rust/Hackathon/UNO-GT/hello-cargo/target/debug/deps/hello_cargo-7fb17f453b82d30a.d: src/main.rs
+
+src/main.rs:
