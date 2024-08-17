a04145dc49363d7d21e674ad20f90b029e4864a9
cormac <cormacflanagan12@gmail.com>
2024-08-17 15:53:08 +1000

    Most Recent Messing with TCP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 pub mod uno;
 use std::io;
-use http::{Request, Response}
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
+    let listner = TcpListener::bind("10.89.212.148:8080").unwrap();
+
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection Established")
+    }
+
     let mut game = uno::Game::new();
     game.init();
     println!("{}", game.debug_player_state());
